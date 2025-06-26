dd3ea38e1bed512feb082a5a508073c7a846cd58
SantiagoH10 <santiago.hernandez-bernal@outlook.com>
2025-06-26 15:40:17 +0000

    my sociabble header

--- memory-game-shipping/src/App.jsx
+++ memory-game-shipping/src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ccLogo from './assets/cma.png'
 import './App.css'
 
+function MySociabble () {
+  return(
+    <div className="bg-ccblue py-12 px-6 text-center">
+      <p className="font-ssp text-white text-xl md:text-3xl font-bold tracking-wide mb-8 max-w-4xl mx-auto leading-tight">
+        WE IMAGINE BETTER WAYS TO SERVE A WORLD IN MOTION
+      </p>
+      <img src={ccLogo} alt="CMA CGM Logo" className="mx-auto h-16 md:h-20 filter brightness-0 invert"/>
+    </div>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MySociabble/>
     </>
   )
 }
--- memory-game-shipping/tailwind.config.js
+++ memory-game-shipping/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ssp': ['Source Sans Pro', 'sans-serif'],
+        'sans': ['Source Sans Pro', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        'ccblue': '#0D173F',
+        'ccred': '#FF0000',
+        'ccaqua': '#3E64B8',
+      }
+    },
   },
   plugins: [],
 }
